42f17b74e88a1d856b3475d5f00ae698df9a3f2a
Prakash Patel <pakpatel@hotmail.com>
2017-02-03 22:48:15 -0600

    Initial commit

---
+++ src/hackMT.UserMgmt/Migrations/20170204034352_FirstMigration.Designer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using hackMT.UserMgmt;
+
+namespace hackMT.UserMgmt.Migrations
+{
+    [DbContext(typeof(TodoDbContext))]
+    [Migration("20170204034352_FirstMigration")]
+    partial class FirstMigration
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431");
+
+            modelBuilder.Entity("hackMT.UserMgmt.TodoItem", b =>
+                {
+                    b.Property<int>("TodoId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Description");
+
+                    b.Property<string>("DueDate");
+
+                    b.HasKey("TodoId");
+
+                    b.ToTable("TodoItems");
+                });
+        }
+    }
+}
---
+++ src/hackMT.UserMgmt/Migrations/20170204034352_FirstMigration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace hackMT.UserMgmt.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoItems",
+                columns: table => new
+                {
+                    TodoId = table.Column<int>(nullable: false)
+                        .Annotation("Autoincrement", true),
+                    Description = table.Column<string>(nullable: true),
+                    DueDate = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoItems", x => x.TodoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoItems");
+        }
+    }
+}
